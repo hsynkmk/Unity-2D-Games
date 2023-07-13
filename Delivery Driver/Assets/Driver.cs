8a073784978af5e7eb6806736f20076e7047a088
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-14 01:35:11 +0300

    Moving object with arrow keys

--- Delivery Driver/Assets/Driver.cs
+++ Delivery Driver/Assets/Driver.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+
     }
 }
