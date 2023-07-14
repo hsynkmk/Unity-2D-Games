0b6c163a75791e075ec96b83f51e8f9c790d2d4f
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-15 00:39:32 +0300

    Destroy objects an change car's color

--- Delivery Driver/Assets/FollowCamera.cs
+++ Delivery Driver/Assets/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] GameObject car;
 
-    // Update is called once per frame
+
     void LateUpdate()
     {
         transform.position = car.transform.position + new Vector3 (0, 0, -10);
