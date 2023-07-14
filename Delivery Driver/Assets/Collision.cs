7676c3b54c8640bafacdf1eae9f6dc3feb8e30b1
Hüseyin Kaymak <huseyinkaymak99@gmail.com>
2023-07-14 03:02:46 +0300

    Assets and follow camera

--- Delivery Driver/Assets/Collision.cs
+++ Delivery Driver/Assets/Collision.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("Collision");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Debug.Log("Trigger");
     }
+
 }
